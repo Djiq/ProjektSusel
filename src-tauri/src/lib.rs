ded8eece090b2798de0fcbf123023007da746da7
Djiq <przwrona@gmail.com>
2024-10-08 20:31:31 +0200

    init stuff and some basic backedn stuff

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,142 @@
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    hash::Hash,
+    io::{Read, Write},
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
+use std::collections;
+
+use dirs::data_dir;
+use serde::{Deserialize, Serialize};
+use tauri::State;
+use tauri_plugin_shell::open;
+
+macro_rules! unwrap_or_err {
+    ( $e:expr, $err:tt ) => {
+        match $e {
+            Ok(x) => x,
+            Err(_) => return Err($err),
+        }
+    };
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Song {
+    name: String,
+    path: String,
+    album: Option<String>,
+    author: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SongDatabase {
+    songs: HashMap<String, Song>,
+}
+
+impl SongDatabase {
+    fn new() -> Result<Self, &'static str> {
+        let mut db = SongDatabase {
+            songs: HashMap::new(),
+        };
+
+        let mut data_dir = match dirs::data_dir() {
+            Some(x) => x,
+            None => return Err("Couldn't access data directory!"),
+        };
+
+        data_dir = data_dir.join("boberplayer");
+        if !data_dir.exists() {
+            unwrap_or_err!(
+                fs::create_dir(data_dir.clone()),
+                "Couldn't create boberplayer data directory!"
+            );
+        }
+
+        let data_file = data_dir.join("songindex.json");
+
+        let mut open_file: File = if !data_file.exists() {
+            unwrap_or_err!(
+                File::create(data_file),
+                "Couldn't create boberplayer music index file!"
+            );
+            return Ok(db);
+        } else {
+            unwrap_or_err!(
+                File::open(data_file),
+                "Couldn't open boberplayer music index file!"
+            )
+        };
+
+        let mut data = String::new();
+        unwrap_or_err!(
+            open_file.read_to_string(&mut data),
+            "Couldn't read data from boberplayer music index file!"
+        );
+
+        db.songs = unwrap_or_err!(
+            serde_json::from_str(&data),
+            "Couldn't deserialize boberplayer music index file!"
+        );
+
+        Ok(db)
+    }
+
+    fn addSong(&mut self, song: Song) -> Result<(), &'static str> {
+        let mut data_dir = match dirs::data_dir() {
+            Some(x) => x,
+            None => return Err("Couldn't access data directory!"),
+        };
+
+        data_dir = data_dir.join("boberplayer");
+        if !data_dir.exists() {
+            unwrap_or_err!(
+                fs::create_dir(data_dir.clone()),
+                "Couldn't create boberplayer data directory!"
+            );
+        }
+
+        let data_file = data_dir.join("songindex.json");
+
+        let mut open_file: File = if !data_file.exists() {
+            unwrap_or_err!(
+                File::create(data_file),
+                "Couldn't create boberplayer music index file!"
+            )
+        } else {
+            unwrap_or_err!(
+                File::open(data_file),
+                "Couldn't open boberplayer music index file!"
+            )
+        };
+
+        self.songs.insert(song.path.to_owned(), song);
+
+        let json = unwrap_or_err!(
+            serde_json::to_string(&self.songs),
+            "Couldn't serialize song data into music index file!"
+        );
+
+        unwrap_or_err!(
+            open_file.write_all(json.as_bytes()),
+            "Writing data to music index file failed!"
+        );
+        Ok(())
+    }
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
